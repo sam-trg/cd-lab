b4bec2173a21c21cde0b0d6bdce5149b34e1a831
sam-trg <samarthasija7@gmail.com>
2025-02-02 22:13:16 +0530

    lab1q2 complete

---
+++ lab1/lab1q2.c
@@ -0,0 +1,28 @@
+// Write a C program to reverse the contents of a file and store in another file.
+// Also display the size of file using file handling function.
+
+#include<stdio.h>
+
+int main(void) {
+
+    FILE* fp1 = fopen("source.txt", "rb");
+    FILE* fp2 = fopen("new.txt", "wb");
+
+    // seek to end
+    // tell for size
+    fseek(fp1, 0, SEEK_END);
+    long int file_size = ftell(fp1);
+
+    printf("Size of the file is %ld bytes\n", file_size);
+
+    // write to another file, rev loop
+    for(long int i = file_size -1; i>=0; i--) {
+        fseek(fp1, i, SEEK_SET);
+        int ch = getc(fp1);
+        putc(ch, fp2);
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+    return 0;
+}
